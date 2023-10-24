049a9290b9e284c70bb58bac5bb6df4aed5e38fb
OKOLIMO-Joseph <okolimofficial@gmail.com>
2023-10-24 22:41:45 +0300

    console.js
    A web page showing the different variable declarations and initialisations in Javascript

---
+++ console.js
@@ -0,0 +1,12 @@
+//Using variables
+
+var name = "Okolimo Joseph";
+console.log(name);
+
+//Two varibles
+var name = "Okolimo Joseph", age = 20;
+console.log("Hey", name, "you are", age, "years old!");
+
+//Or Using Identifiers
+var name = "Okolimo Joseph", age = 20;
+console.log("Hey %s, you are %s years old", name, age);
