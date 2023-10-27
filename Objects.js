ff76ade91ac2e95b135cc325c9817e67b5ac63b3
OKOLIMO-Joseph <okolimofficial@gmail.com>
2023-10-27 16:33:50 +0300

    Object.js
    Objects in Javascript

---
+++ Objects.js
@@ -0,0 +1,10 @@
+let credentials = {
+    name: 'OKOLIMO-Joseph',
+    campus: 'UCU-Main-Campus',
+    course: 'BSIT',
+    courseunit: 'Web-And-Mobile-App-Dev',
+    year: 'Year-One',
+    semester: 'Two'
+};
+console.log(credentials);
+
