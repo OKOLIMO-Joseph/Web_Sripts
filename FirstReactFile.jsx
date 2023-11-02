7b43e7f700f0a10cd634fb1971415f31795f55f8
OKOLIMO-Joseph <okolimofficial@gmail.com>
2023-11-02 22:04:51 +0300

    FirstReactFile.jsx
    my first react file showing my code journey

---
+++ FirstReactFile.jsx
@@ -0,0 +1,16 @@
+export default function myName(){
+    return (
+        <div>
+            <h2>My Programming Journey</h2>
+            <ol>
+                <li>Java</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>C-Lamguage</li>
+                <li>Python</li>
+                <li>Javascript</li>
+                <li>React</li>
+            </ol>
+        </div>
+    )
+}
