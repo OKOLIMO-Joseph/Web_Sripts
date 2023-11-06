d48238eb16308ee6bac47ad30ad2634aa7498ed1
OKOLIMO-Joseph <okolimofficial@gmail.com>
2023-11-06 22:53:28 +0300

    App.jsx
    React code for the react app

---
+++ Calculator/ReactApp/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function App(){
+  return(
+    <div>
+      <h1>0</h1>
+      <button>+</button>
+      <button>STOP</button>
+      <button>-</button>
+    </div>
+  )
+}
+export default App;
