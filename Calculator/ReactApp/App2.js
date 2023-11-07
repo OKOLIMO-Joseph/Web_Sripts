29c180a93eb6e13f96f2906b9fd9b12b110db12f
OKOLIMO-Joseph <okolimofficial@gmail.com>
2023-11-07 21:30:17 +0300

    App2.js
    React code for the counter

---
+++ Calculator/ReactApp/App2.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './ReactScripts/style.css';
+
+function App() {
+  const [counter, setCount] = useState(0)
+  
+  function increment(){
+    setCount(counter+1);
+  }
+  function decrement(){
+    setCount(counter-1);
+  }
+  function stop(){
+    setCount(0);
+  }
+  function msg(){
+    alert("Built by OKOLIMO-Joseph, NINSIIMA-Whitney, MWINE-Irwin and SSEKISONGE-Robert")
+  }
+  
+return (
+  <div id="main">
+    <h1>Simple On-Click Counter</h1>
+    <div id="head"><h2>{counter}</h2></div>
+<button onClick= {increment} id="forward" >+</button>
+<button onClick= {stop} id="stop"> Stop </button>
+<button onClick= {decrement} id="back">-</button>
+<div>
+  <button onClick= {msg} id="members">View Group Members!</button>
+</div>
+
+<div id="footer">
+  <footer>#code_GEEKS
+    
+  </footer>
+  </div>
+  </div>
+);
+}
+
+export default App;
