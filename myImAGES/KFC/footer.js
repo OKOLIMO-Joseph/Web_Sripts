93249f58a8e9d1997f7aa81c604411fa5da51b87
OKOLIMO-Joseph <okolimofficial@gmail.com>
2023-11-24 19:16:28 +0300

    KFC App
    An app deisgned in React And CSS for KFC

---
+++ myImAGES/KFC/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const footer = ({showing}) => {
+  return (
+    <div>
+        <div id='about'>
+            <h2>About Us</h2>
+            <span>We are located at Mukono </span>
+        </div>
+    </div>
+  )
+}
---
+++ myImAGES/KFC/src/header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+
+function Header({showing}) {
+  //host this in butter
+//   burron state
+    const [show, setShow] = React.useState(false);
+  return (
+    <div>
+      <div className="d">
+        <h1 className="header" style={{fontFamily: "castellar", fontSize: 50}}>KFC</h1>
+        {/* view cart button */}
+        <button href="/cart" className="cart-btn" onClick={()=>{setShow(!show)
+        showing(!show)
+        }}>
+          {show? "close": "View Cart"}
+        </button>
+      </div>
+      
+      
+    </div>
+  );
+}
+
+export default Header;
