ee116881e5cae27c326939bac18eb9b28b255b70
Joseph_OKOLIMO <137505067+OKOLIMO-Joseph@users.noreply.github.com>
2024-10-06 12:41:12 +0300

    For Loop in C

---
+++ C-Programming/ForLoop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+   int i, j = 0;
+    for (i = 1; i <= 10; i++) {
+        printf("i = %d%d\n", i,j);
+    } 
+
+
+   /* int i, j;
+    for (i = 1; j = 0; i < 10, j < 3; i++) {
+        printf("Output = %d%d\n", i,j);
+    } */
+
+
+    return 0;
+}
