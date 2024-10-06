6301b195ceb73fd63590daef5b7fb039dac76314
Joseph_OKOLIMO <137505067+OKOLIMO-Joseph@users.noreply.github.com>
2024-10-06 12:49:05 +0300

    Program that shows how to use the different maths operations in C

---
+++ C-Programming/MathsOperations.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>  // Endavour to include this when daeling with advanced math operations
+int main(){
+double A;  //ie A =sqrt(9);   square roots
+double B;  //ie B = pow(2);   power
+int C;     //ie C = round(3.14);  rounding off a number
+int D;     //ie  D = ceil(3.14);  rounding to the nearest forward number
+int E;     //ie  E = floor(3.99);  rounding to the nearest backward number
+double F;  //ie  F = fabs(-100);
+double G;  //ie  G = log(3);
+double H;  //ie  H = sin(45)
+double I;  //ie  I = cos(45)
+double J;  //ie  J = tan(45)
+
+//A = sqrt(49);
+//E = ceil(4.678);
+G = log(45);
+printf("%lf", G);  //The %lf and a within the printf function is variable depending on the data type and variable name
+
+    return 0;
+}
