5ac8bf9beff670c662e19c168c3847e21163a5e7
Joseph_OKOLIMO <137505067+OKOLIMO-Joseph@users.noreply.github.com>
2024-10-07 09:45:13 +0300

    Program in C that calculates bills

---
+++ C-Programming/BillCalc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int Consultation = 100000, LabTest = 350000, Admission = 20000, Oxygen = 200000, Treatment = 800000, ICU = 1200000;
+
+    printf("SERVICES OFFERED AT MM INTERNATIONAL HOSPITAL \n");
+    printf("SERVICE\t         PRICE \n");
+    printf("1. Consultation\t 100000 \n");
+    printf("2. Admission\t 350000 \n");
+    printf("3. Lab Tests\t 20000 \n");
+    printf("4. Treatment\t 200000 \n");
+    printf("5. Oxygen\t 800000 \n");
+    printf("6. ICU\t         1200000 \n");
+    printf("\n");
+    
+    
+
+    return 0;
+}
