14d4d13db03e2e4aab1b75ef73a9308899a07315
Joseph_OKOLIMO <137505067+OKOLIMO-Joseph@users.noreply.github.com>
2024-10-07 09:51:48 +0300

    Designing shapes in C

---
+++ C-Programming/Shapes.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    printf("Rectangle \n");
+    printf("|");
+    printf("-");
+    printf("-");
+    printf("-");
+    printf("-");
+    printf("-");
+    printf("-");
+    printf("-");
+    printf("-");
+    printf("|");
+    printf("-");
+    printf("-");
+    printf("-");
+    printf("-");
+    printf("-");
+    printf("-");
+    printf("-");
+    printf("-");
+    
+
+
+
+    return 0;
+}
